9e5dd04fe9f43edef1a43f334e956f4a97ad4742
sneurlax <sneurlax@gmail.com>
2022-12-29 17:09:32 -0600

    don't build monero
    don't know don't care

--- scripts/linux/build_sharedfile.sh
+++ scripts/linux/build_sharedfile.sh
@@ -11,15 +11,15 @@ COMMIT=$(git log -1 --pretty=format:"%H")
 OS="LINUX"
 sed -i "/\/\*${OS}_VERSION/c\\/\*${OS}_VERSION\*\/ const ${OS}_VERSION = \"$COMMIT\";" $VERSIONS_FILE
 cd build
-mkdir monero_build
+# mkdir monero_build
 mkdir wownero_build
-MONERO_BUILD=$(pwd)/monero_build
+# MONERO_BUILD=$(pwd)/monero_build
 WOWNERO_BUILD=$(pwd)/wownero_build
 
-cd $MONERO_BUILD
-cmake ../../cmakefiles/monero/${TYPES_OF_BUILD}
-make -j$(nproc)
-cp libcw_monero.so ../
+# cd $MONERO_BUILD
+# cmake ../../cmakefiles/monero/${TYPES_OF_BUILD}
+# make -j$(nproc)
+# cp libcw_monero.so ../
 
 cd $WOWNERO_BUILD
 cmake ../../cmakefiles/wownero/${TYPES_OF_BUILD}
