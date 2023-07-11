a6afd6ecd197f1039c5c702149566b4ad7f17a0b
fossephate <matt.cfosse@gmail.com>
2023-07-11 16:33:23 -0400

    windows build fix

--- scripts/windows/config.sh
+++ scripts/windows/config.sh
@@ -9,7 +9,7 @@ export ANDROID_NDK_ROOT=${WORKDIR}/android-ndk-r17c
 export ANDROID_NDK_HOME=$ANDROID_NDK_ROOT
 export TOOLCHAIN_DIR="${WORKDIR}/toolchain"
 export TOOLCHAIN_BASE_DIR=$TOOLCHAIN_DIR
-export ORIGINAL_PATH=$PATH
+export ORIGINAL_PATH="$PATH"
 export THREADS=$(nproc)
 export TYPES_OF_BUILD="x86_64"
 
